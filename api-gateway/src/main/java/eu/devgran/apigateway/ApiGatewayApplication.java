8f9f3cb66236d53ac12446ec439b4510b77832fd
d-gran <soft.grankowski@gmail.com>
2023-06-25 22:52:43 +0200

    Removed Zuul since its no longer actively supported by Spring Cloud. Added Spring Cloud Gateway instead

--- api-gateway/src/main/java/eu/devgran/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/eu/devgran/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,10 @@
-package eu.devgran.servicediscovery;
+package eu.devgran.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
