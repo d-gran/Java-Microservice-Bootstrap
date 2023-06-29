17ddad2a319b053baa324854118b6c4dfe5b97a9
d-gran <soft.grankowski@gmail.com>
2023-06-29 23:06:16 +0200

    Added simple Circuit Breaker pattern example (Resilience4j)

--- second-business-service/src/main/java/eu/devgran/secondbusinesservice/service/DefaultSecondBusinessService.java
+++ second-business-service/src/main/java/eu/devgran/secondbusinesservice/service/DefaultSecondBusinessService.java
@@ -1,5 +1,6 @@
 package eu.devgran.secondbusinesservice.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,12 @@ public class DefaultSecondBusinessService implements SecondBusinessService {
     private final FirstBusinessServiceClient firstBusinessServiceClient;
 
     @Override
+    @CircuitBreaker(name = "first-business-service-caller", fallbackMethod = "callFirstFallback")
     public String callFirst() {
         return firstBusinessServiceClient.callFirst();
+    }
+
+    public String callFirstFallback(Exception e) {
+        return "Circuit breaker triggered. Retry in a while...";
     }
 }
