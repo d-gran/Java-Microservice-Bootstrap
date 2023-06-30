ced3e71ec3aecc3ec5099ece2306a98d1014461a
d-gran <soft.grankowski@gmail.com>
2023-06-30 19:45:38 +0200

    Added working tracing with Micrometer & Zipkin integration

--- second-business-service/src/main/java/eu/devgran/secondbusinesservice/controller/SecondBusinessServiceController.java
+++ second-business-service/src/main/java/eu/devgran/secondbusinesservice/controller/SecondBusinessServiceController.java
@@ -2,11 +2,9 @@ package eu.devgran.secondbusinesservice.controller;
 
 import eu.devgran.secondbusinesservice.service.SecondBusinessService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class SecondBusinessServiceController {
